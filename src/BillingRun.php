db017d079c5aa8c57b0c53bd8169c42d7c17e34e
Christopher Hertel <christopher.hertel@sensiolabs.de>
2018-05-02 19:06:42 +0200

    move output to logging

--- src/BillingRun.php
+++ src/BillingRun.php
@@ -11,8 +11,7 @@ use App\Invoice\Mailer;
 use App\Payment\Exception as PaymentException;
 use App\Payment\Provider as PaymentProvider;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface;
 
 class BillingRun
 {
@@ -20,28 +19,31 @@ class BillingRun
     private $paymentProvider;
     private $mailer;
     private $exporter;
+    private $logger;
 
     public function __construct(
         EntityManagerInterface $entityManager,
         PaymentProvider $paymentProvider,
         Mailer $mailer,
-        Exporter $exporter
+        Exporter $exporter,
+        LoggerInterface $logger
     ) {
         $this->entityManager = $entityManager;
         $this->paymentProvider = $paymentProvider;
         $this->mailer = $mailer;
         $this->exporter = $exporter;
+        $this->logger = $logger;
     }
 
-    public function start(\DateTimeImmutable $period, OutputInterface $output): void
+    public function start(\DateTimeImmutable $period): void
     {
         $customers = $this->fetchActiveCustomer();
-        $output->writeln(sprintf('<info>Loaded %d customers to process</info>'.PHP_EOL, count($customers)));
+        $this->logger->info(sprintf('Loaded %d customers to process', count($customers)));
 
-        $invoices = $this->generateInvoice($output, $customers, $period);
-        $this->payInvoices($output, $invoices);
-        $this->sendInvoices($output, $invoices);
-        $this->exportMagazines($output, $period, $invoices);
+        $invoices = $this->generateInvoice($customers, $period);
+        $this->payInvoices($invoices);
+        $this->sendInvoices($invoices);
+        $this->exportMagazines($period, $invoices);
     }
 
     /**
@@ -55,79 +57,56 @@ class BillingRun
     /**
      * @return Invoice[]
      */
-    private function generateInvoice(OutputInterface $output, array $customers, \DateTimeImmutable $period): array
+    private function generateInvoice(array $customers, \DateTimeImmutable $period): array
     {
-        $output->writeln('Generate Invoices:');
-
         $invoices = [];
-        $progressBar = new ProgressBar($output, count($customers));
-        $progressBar->start();
         foreach ($customers as $i => $customer) {
             $invoice = Invoice::forCustomer($customer, $period);
             $invoices[] = $invoice;
             $this->entityManager->persist($invoice);
-            $progressBar->advance();
         }
-        $progressBar->finish();
-
-        $output->writeln('');
-        $output->writeln(sprintf('<comment>Generated %d invoices to pay</comment>'.PHP_EOL, count($invoices)));
+        $this->logger->debug(sprintf('Generated %d invoices to pay', count($invoices)));
 
         $this->entityManager->flush();
 
         return $invoices;
     }
 
-    private function payInvoices(OutputInterface $output, array $invoices): void
+    private function payInvoices(array $invoices): void
     {
-        $output->writeln('Authorize invoices:');
-
-        $progressBar = new ProgressBar($output, count($invoices));
-        $progressBar->start();
         $success = 0;
         foreach ($invoices as $invoice) {
             try {
                 $this->paymentProvider->authorize($invoice);
                 ++$success;
             } catch (PaymentException $exception) {
-                $output->writeln(
-                    sprintf('<error>An error occurred while authorizing payment for invoice %s</error>', $invoice)
+                $this->logger->error(
+                    sprintf('An error occurred while authorizing payment for invoice %s', $invoice)
                 );
             }
-
-            $progressBar->advance();
         }
         $this->entityManager->flush();
 
-        $progressBar->finish();
-
-        $output->writeln('');
-        $output->writeln(sprintf('<comment>Authorized %d invoices</comment>'.PHP_EOL, $success));
+        $this->logger->debug(sprintf('Authorized %d invoices', $success));
     }
 
-    private function sendInvoices(OutputInterface $output, $invoices): void
+    private function sendInvoices($invoices): void
     {
-        $output->writeln('Generate PDFs and send notifications invoices:');
+        $this->logger->debug('Generate PDFs and send notifications invoices:');
 
-        $progressBar = new ProgressBar($output, count($invoices));
-        $progressBar->start();
         foreach ($invoices as $invoice) {
             $this->mailer->sendInvoice($invoice);
-            $progressBar->advance();
         }
 
-        $progressBar->finish();
-
-        $output->writeln('');
-        $output->writeln(sprintf('<comment>Sent %d invoices</comment>'.PHP_EOL, count($invoices)));
+        $this->logger->debug(sprintf('Sent %d invoices', count($invoices)));
     }
 
-    private function exportMagazines(OutputInterface $output, \DateTimeImmutable $period, array $invoices): void
+    private function exportMagazines(\DateTimeImmutable $period, array $invoices): void
     {
-        $output->write('Export invoices: ');
+        $this->logger->debug('Export invoices: Start');
 
         $this->exporter->export($period, $invoices);
 
-        $output->writeln('<info>OK</info>');
+        $this->logger->debug('Export invoices: Done');
     }
 }
